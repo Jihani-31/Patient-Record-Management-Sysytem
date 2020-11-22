383f880326cff1400defac1637ec80636ef72bc8
Jihani-31 <jihaninanayakkara@gmail.com>
2020-11-23 02:07:18 +0530

    updated login

--- inc/connection.php
+++ inc/connection.php
@@ -1,20 +1,18 @@
-<?php
- 
-//$connection=mysqli_connect(server,user,pass,name)
-$db=mysqli_connect('localhost','root','','userdb');
+<?php 
 
-//   mysqli_connect_errno();             checking connection
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = '';
+	$dbname = 'userdb'; 
 
+	$connection = mysqli_connect('localhost', 'root', '', 'userdb');
 
+	// Checking the connection
+	if (mysqli_connect_errno()) {
+		die('Database connection failed ' . mysqli_connect_error());
+	}
+	else{
+		// echo "connection succeful";
+	}
 
-if(mysqli_connect_errno())
-{
-    die('Database connection failed' .mysqli_connect_error());
-}
-else{
-   // echo "connection successful";
-
-}
-
- 
- ?>
+?>
--- index.php
+++ index.php
@@ -1,38 +1,93 @@
-<?php
-	require_once('inc/connection.php');
+<?php session_start(); ?>
+<?php require_once('inc/connection.php'); ?>
+<?php 
 
+	// check for form submission
+	if (isset($_POST['submit'])) {
+
+		$errors = array();
+
+		// check if the username and password has been entered
+		if (!isset($_POST['email']) || strlen(trim($_POST['email'])) < 1 ) {
+			$errors[] = 'Username is Missing / Invalid';
+		}
+
+		if (!isset($_POST['password']) || strlen(trim($_POST['password'])) < 1 ) {
+			$errors[] = 'Password is Missing / Invalid';
+		}
+
+		// check if there are any errors in the form
+		if (empty($errors)) {
+			// save username and password into variables
+			$email 		= mysqli_real_escape_string($connection, $_POST['email']);
+			$password 	= mysqli_real_escape_string($connection, $_POST['password']);
+			$hashed_password = sha1($password);
+
+			// prepare database query
+			$query = "SELECT * FROM user 
+						WHERE email = '{$email}' 
+						AND password = '{$hashed_password}' 
+						LIMIT 1";
+
+			$result_set = mysqli_query($connection, $query);
+
+			if ($result_set) {
+				// query succesfful
+
+				if (mysqli_num_rows($result_set) == 1) {
+					// valid user found
+					// redirect to users.php
+					header('Location: users.php');
+				} else {
+					// user name and password invalid
+					$errors[] = 'Invalid Username / Password';
+				}
+			} else {
+				$errors[] = 'Database query failed';
+			}
+		}
+	}
 ?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<title>Log-In</title>
+	<meta charset="UTF-8">
+	<title>Log In - User Management System</title>
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 	<div class="login">
+
 		<form action="index.php" method="post">
+			
 			<fieldset>
 				<legend><h1>Log In</h1></legend>
-				<!-- <p class="error">Invalid username or Password</p> -->
+
+				<?php 
+					if (isset($errors) && !empty($errors)) {
+						echo '<p class="error">Invalid Username / Password</p>';
+					}
+				?>
+
 				<p>
 					<label for="">Username:</label>
-					<input type="email" name="email" placeholder="Email">
+					<input type="text" name="email" id="" placeholder="Email Address">
 				</p>
+
 				<p>
 					<label for="">Password:</label>
-					<input type="password" name="password" placeholder="Password">
+					<input type="password" name="password" id="" placeholder="Password">
 				</p>
-				<p>
-					<button type="submit" name="submit">LogIn</button>
-				</p>
-			</fieldset>
-		</form>
-	</div>
 
+				<p>
+					<button type="submit" name="submit">Log In</button>
+				</p>
+
+			</fieldset>
+
+		</form>		
+
+	</div> <!-- .login -->
 </body>
 </html>
-
-
-<?php mysqli_close($db); ?>
+<?php mysqli_close($connection); ?>
