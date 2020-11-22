518464ac118960e23cd56be709d13c2b2a16d0cd
Jihani-31 <jihaninanayakkara@gmail.com>
2020-11-23 00:47:22 +0530

    updated css login

--- index.php
+++ index.php
@@ -7,9 +7,29 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<title>Log-In</title>
 </head>
 <body>
+	<div class="login">
+		<form action="index.php" method="post">
+			<fieldset>
+				<legend><h1>Log In</h1></legend>
+				<!-- <p class="error">Invalid username or Password</p> -->
+				<p>
+					<label for="">Username:</label>
+					<input type="email" name="email" placeholder="Email">
+				</p>
+				<p>
+					<label for="">Password:</label>
+					<input type="password" name="password" placeholder="Password">
+				</p>
+				<p>
+					<button type="submit" name="submit">LogIn</button>
+				</p>
+			</fieldset>
+		</form>
+	</div>
 
 </body>
 </html>
